c2aefaa7811ccd3a94e3c061515e4f30ec5c1c15
Alberto Bustillos <aalbertobus@gmail.com>
2022-03-26 12:30:01 -0700

    Update ifelse.js

--- ifelse.js
+++ ifelse.js
@@ -1,5 +1,5 @@
+//IF ELSE EJEMPLO
 var yadesayuno = false 
-
 
 //If significa si pasa esto.... 
 //Else es todo lo demás
@@ -8,4 +8,23 @@ if(yadesayuno == true){
     console.log("Magio ya desayuno")
 } else {
     console.log("Magio ya no desayuno aún ")
+}
+
+
+
+
+//ELSE IF EJEMPLO
+var envio = "estafeta"
+
+
+if(envio == "estafeta"){
+ console.log("Mandando al sitio de estafeta")
+} else if(envio == "redpack"){
+console.log("Mandando al sitio de redpack")
+} else if(envio == "sepomex"){
+console.log("Mandando al sitio de sepomex")
+}else if(envio == "persona"){
+  console.log("ya vinieron por el paquete")
+} else {
+console.log("El paquete se perdió")
 }
