4fee202b9e13fe3de88bf6a1a55cb14fbb3154e6
Alberto Bustillos <aalbertobus@gmail.com>
2022-03-26 12:17:26 -0700

    If Else

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+var yadesayuno = false 
+
+
+//If significa si pasa esto.... 
+//Else es todo lo demás
+
+if(yadesayuno == true){
+    console.log("Magio ya desayuno")
+} else {
+    console.log("Magio ya no desayuno aún ")
+}
