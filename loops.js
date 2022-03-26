8c7cff1d796dc930d6e7250a3e205e6e2e0de726
Alberto Bustillos <aalbertobus@gmail.com>
2022-03-26 13:14:04 -0700

    for loop

---
+++ loops.js
@@ -0,0 +1,9 @@
+//FOR LOOP
+
+var carros = ["rojo", "Verde", "azul", "rosa", "amarillo"];
+
+//for(indicecontador, condicion, incremento){ //acciion }
+
+for(var i=0; i<carros.length; i++){
+ console.log("tenemos el carro " + carros[i])
+}
